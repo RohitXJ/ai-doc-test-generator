feea623431e9d445925a474d60903129ee9b4fec
Rohit Gomes <gomesrohit92@gmail.com>
2026-02-05 20:26:11 +0530

    Update diff_extractor.py

--- src/diff_extractor.py
+++ src/diff_extractor.py
@@ -1,9 +1,20 @@
 import subprocess
+import os
 
 def get_python_diff():
+    base = os.environ.get("GITHUB_BASE_REF")
+
+    if not base:
+        # fallback for non-PR runs
+        cmd = ["git", "diff", "HEAD~1", "--", "*.py"]
+    else:
+        # PR run (this is your case)
+        cmd = ["git", "diff", f"origin/{base}...HEAD", "--", "*.py"]
+
     result = subprocess.run(
-        ["git", "diff", "origin/main...HEAD", "--", "*.py"],
+        cmd,
         capture_output=True,
         text=True
     )
+
     return result.stdout.strip()
